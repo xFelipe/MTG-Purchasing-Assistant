4bf9ec11de1346abd33337d8c84fa4ef1f32d1c7
xFelipe <felipegdmito@gmail.com>
2019-07-22 02:12:06 -0300

    Segunda tentativa de deploy

--- mtgpa/settings.py
+++ mtgpa/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v5gink6_u$ynu35qfn@e_x654tqyf&fv57=-+7r&f)-z+b1t-+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
