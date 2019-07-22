1b893ec55a96ed0b91ae0779234f9f05d9b782b5
xFelipe <felipegdmito@gmail.com>
2019-07-22 02:29:06 -0300

    wsgi

--- mtgpa/wsgi.py
+++ mtgpa/wsgi.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 from dj_static import Cling
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtgpa.settings')
