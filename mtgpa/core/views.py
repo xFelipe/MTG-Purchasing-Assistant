c02087817cbc0ee03ad03d4446ad65100c1d41fe
xFelipe <felipegdmito@gmail.com>
2019-07-22 19:23:24 -0300

    Teste

--- mtgpa/core/views.py
+++ mtgpa/core/views.py
@@ -11,16 +11,17 @@ store_card_link_base = 'https://ligamagic.net'
 
 
 def home(request):
-    if request.method != 'POST':
-        return render(request, 'home.html', {'form': CardsForm()})
-    else:
-        content = request.POST['cards'].split('\r\n')
-        precision = int(request.POST['precision']) if(request.POST['precision']) else None
-        cards, market = search_engine(content, precision)
-
-        response = {'cards': cards, 'form': CardsForm(request.POST),
-                    'markets': sort_market(market)}
-        return render(request, 'home.html', response)
+    # if request.method != 'POST':
+    #     return render(request, 'home.html', {'form': CardsForm()})
+    # else:
+    #     content = request.POST['cards'].split('\r\n')
+    #     precision = int(request.POST['precision']) if(request.POST['precision']) else None
+    #     cards, market = search_engine(content, precision)
+    #
+    #     response = {'cards': cards, 'form': CardsForm(request.POST),
+    #                 'markets': sort_market(market)}
+    #     return render(request, 'home.html', response)
+    return render(request, 'home.html')
 
 
 def search_engine(content, precision):
